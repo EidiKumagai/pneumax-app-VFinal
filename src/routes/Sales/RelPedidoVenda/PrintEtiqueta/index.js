3f7e530a1e0414aa426f43daa8c210aff54307ed
levywinnikes <levy.winnikes@gmail.com>
2021-09-29 20:01:43 -0300

    Atualização em etiquetas

--- src/routes/Sales/RelPedidoVenda/PrintEtiqueta/index.js
+++ src/routes/Sales/RelPedidoVenda/PrintEtiqueta/index.js
@@ -2,7 +2,8 @@ import React, { Fragment, Component } from "react";
 import ReactToPrint from "react-to-print";
 import { Button, Col, Row, Card } from "antd";
 import "./style.css";
-import imgSvg from "./knapp_logo.svg";
+import LogoKnap from "./knapp_logo.svg";
+import Barcode from "react-barcode";
 import moment from "moment";
 const numero = require("numero-por-extenso");
 
@@ -111,7 +112,18 @@ export class PrintComponent extends React.Component {
                         height: "50%"
                       }}
                     >
-                      <Col span={24}>Logo</Col>
+                      <Col span={24}>
+                        <Row
+                          style={{
+                            alignItems: "center",
+                            justifyContent: "center",
+                            height: "100%",
+                            padding: "12px"
+                          }}
+                        >
+                          <img src={LogoKnap}></img>
+                        </Row>
+                      </Col>
                     </Row>
 
                     <Row
@@ -120,29 +132,122 @@ export class PrintComponent extends React.Component {
                         borderTop: "1px solid #000"
                       }}
                     >
-                      <Col span={24}>End: </Col>
+                      <Col span={24}>
+                        <p
+                          style={{
+                            fontSize: "35px",
+                            paddingLeft: "12px"
+                          }}
+                        >
+                          End:
+                        </p>
+                      </Col>
                     </Row>
                   </Col>
                   <Col span={16}>
                     <Row
                       style={{
-                        height: "20%",
+                        height: "30%",
                         borderTop: "1px solid #000",
+                        borderRight: "1px solid #000",
+
                         borderBottom: "1px solid #000",
                         borderLeft: "1px solid #000"
                       }}
                     >
-                      <Col span={24}>Projeto:</Col>
+                      <Col span={24}>
+                        <Row
+                          style={{
+                            alignItems: "center",
+                            height: "100%"
+                          }}
+                        >
+                          <p
+                            style={{
+                              fontSize: "35px",
+                              paddingLeft: "12px",
+                              paddingTop: "18px"
+                            }}
+                          >
+                            Projeto: 3.284 PKS-014-18-DIM-BR-CWB
+                          </p>
+                        </Row>
+                      </Col>
                     </Row>
                     <Row
                       style={{
-                        height: "80%",
+                        height: "70%",
                         borderRight: "1px solid #000",
 
                         borderLeft: "1px solid #000"
                       }}
                     >
-                      <Col span={24}>Ord. Compra: </Col>
+                      <Col span={24}>
+                        <Row>
+                          <Col span={10}>
+                            <p
+                              style={{
+                                fontSize: "35px",
+                                paddingLeft: "6px",
+                                paddingTop: "18px"
+                              }}
+                            >
+                              Ord. Compra:
+                            </p>
+                          </Col>
+                          <Col span={14}>
+                            <Row
+                              style={{
+                                height: "30%",
+                                display: "flex",
+                                justifyContent: "space-between"
+                              }}
+                            >
+                              <p
+                                style={{
+                                  fontSize: "35px",
+
+                                  paddingTop: "18px"
+                                }}
+                              >
+                                29.995
+                              </p>
+                              <p
+                                style={{
+                                  fontSize: "35px",
+
+                                  paddingTop: "18px"
+                                }}
+                              >
+                                Seq.:
+                              </p>
+                              <p
+                                style={{
+                                  fontSize: "35px",
+
+                                  paddingTop: "18px"
+                                }}
+                              >
+                                1
+                              </p>
+                            </Row>
+                          </Col>
+                        </Row>
+                        <Row
+                          style={{
+                            height: "70%"
+                          }}
+                        >
+                          <Col span={24}>
+                            <Barcode
+                              margin={0}
+                              displayValue={false}
+                              height={"80%"}
+                              value="468464646afsasf684asf64846"
+                            ></Barcode>
+                          </Col>
+                        </Row>
+                      </Col>
                     </Row>
                   </Col>
                 </Row>
@@ -151,11 +256,52 @@ export class PrintComponent extends React.Component {
                     height: "40%",
                     borderTop: "1px solid #000",
                     borderRight: "1px solid #000",
-                    borderBottom: "1px solid #000",
                     borderLeft: "1px solid #000"
                   }}
                 >
-                  <Col span={24}>Produto: Eixo Zincado</Col>
+                  <Col span={24}>
+                    <Row>
+                      <p
+                        style={{
+                          fontSize: "35px",
+                          paddingLeft: "24px",
+
+                          paddingTop: "24px"
+                        }}
+                      >
+                        Produto:
+                      </p>
+                      <p
+                        style={{
+                          paddingTop: "12px",
+                          fontSize: "46px",
+                          paddingLeft: "64px"
+                        }}
+                      >
+                        4648646846457
+                      </p>
+                    </Row>
+                    <Row>
+                      <p
+                        style={{
+                          fontSize: "35px",
+                          paddingLeft: "24px"
+                        }}
+                      >
+                        EIXO TRANSV.TIPO1STRNB-390
+                      </p>
+                    </Row>
+                    <Row>
+                      <p
+                        style={{
+                          fontSize: "35px",
+                          paddingLeft: "24px"
+                        }}
+                      >
+                        ZINCADO
+                      </p>
+                    </Row>
+                  </Col>
                 </Row>
 
                 <Row
@@ -170,15 +316,34 @@ export class PrintComponent extends React.Component {
                   <Col
                     span={16}
                     style={{
-                      borderTop: "1px solid #000",
-                      borderRight: "1px solid #000",
-                      borderBottom: "1px solid #000",
-                      borderLeft: "1px solid #000"
+                      borderRight: "1px solid #000"
                     }}
                   >
-                    Barcode
+                    <Row
+                      style={{
+                        alignItems: "center",
+                        height: "100%",
+                        paddingLeft: "48px"
+                      }}
+                    >
+                      <Barcode
+                        margin={0}
+                        displayValue={false}
+                        height={"100%"}
+                        value="468464646afsasf684asf64846"
+                      ></Barcode>
+                    </Row>
                   </Col>
-                  <Col span={8}>Qtd</Col>
+                  <Col span={8}>
+                    <p
+                      style={{
+                        fontSize: "35px",
+                        paddingLeft: "24px"
+                      }}
+                    >
+                      Qtd
+                    </p>
+                  </Col>
                 </Row>
               </Col>
             </Row>
